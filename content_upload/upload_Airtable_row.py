7f5e8271d3cf4733f019fd0bec547cde8902e4be
tyron-pretorius <70140496+tyron-pretorius@users.noreply.github.com>
2020-10-20 05:46:37 -0500

    Update upload_Airtable_row.py

--- content_upload/upload_Airtable_row.py
+++ content_upload/upload_Airtable_row.py
@@ -12,7 +12,7 @@ import json
 content_type = {"Case Study": ["recjkV3BC4G1ivVND","Case%20Studies"] , "eBook":["recjAroCT0KNRuHC2","eBooks"], "Fact Sheet":["recRq8P5MHkeK6ZDV","Fact%20Sheets"], "Infographic":["reckG4aGBdkqtYB2z", "Infographics"], "Guide":["recmNFMoRiuE7IF2j", "Other"], "Whitepaper":["recxrhA9kyyGDRES7","Other"]}
 
 c_type = input['c_type']
-post_url = "https://api.airtable.com/v0/apppRyhNabr5LTYeR/"+content_type[c_type][1]
+post_url = "https://api.airtable.com/v0/xxx/"+content_type[c_type][1]
 rebrandly= 'https://'+input['rebrandly']
 c_name = input['c_name']
 
