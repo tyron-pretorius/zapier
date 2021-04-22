1eb876fe0869178bb984ed06de6e57934206144d
tyron-pretorius <70140496+tyron-pretorius@users.noreply.github.com>
2021-04-22 08:08:11 -0500

    Create send_rows.js

---
+++ google_sheets_import/send_rows.js
@@ -0,0 +1,50 @@
+function sendRows() {
+  
+
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Customer Database");
+  var rows = sheet.getDataRange().getValues();
+
+  var names_string="";
+  var orders_string="";
+  var emails_string="";
+  var phones_string="";
+
+  for (var i=1;i<rows.length;i++){
+
+    if(rows[i][4]==true){
+
+      if (names_string==""){
+        names_string = rows[i][1];
+        orders_string = rows[i][2];
+        emails_string = rows[i][3];
+        phones_string = rows[i][4];
+      }
+      else{
+        names_string = names_string+"*"+rows[i][1];
+        orders_string = orders_string+"*"+rows[i][2];
+        emails_string = emails_string+"*"+rows[i][3];
+        phones_string = phones_string+"*"+rows[i][4];
+      }
+
+
+    }
+  }
+  
+  var ts = new Date().toLocaleString()
+      
+  var url = "https://hooks.zapier.com/hooks/catch/65051/ojrsspj/";
+  var options = {
+    "method": "post",
+    "headers": {},
+    "payload": {
+      "Timestamp": ts,
+      "Names": names_string,
+      "Orders": orders_string,
+      "Emails":emails_string,
+      "Phones":phones_string
+    }
+  };
+  
+  var response = UrlFetchApp.fetch(url, options);
+ 
+}
